e3bfb2fdf7050712cf504d28a9e10a7a7c96676b
B2Yassine00 <banouq90@gmail.com>
2024-02-15 19:16:54 +0100

    made the api gateway to my microservices architecture

---
+++ src/main/java/com/microservices/learning/apigateway/GatewayConfiguration.java
@@ -0,0 +1,8 @@
+package com.microservices.learning.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class GatewayConfiguration {
+
+}
